fb1bc6fdef446d11bfcd8e720e17eb17cefbca2c
Jorge Nascimento <jorg3nascimento@gmail.com>
2021-05-20 14:49:59 -0300

    Finalizado projeto Event KeyCodes

---
+++ 11 -  Event KeyCodes/README.md
@@ -0,0 +1,7 @@
+<h1 align="center"> Sound Board Project </h1>
+
+<p align="center"> Projeto de criação de app manipula o evento de pressionar a tecla buscando informações sobre a tecla que foi pressionado, como qual tecla, qual o código da tecla ..</p>
+
+</br> <hr>
+
+<p align = "center"><img src= "./.github/Event_Keys.gif"></p>
---
+++ 11 -  Event KeyCodes/script.js
@@ -0,0 +1,63 @@
+/**
+ * <div class="view">
+        Press any key to get the KeyCode <br />
+        <div class="box">
+          <h4>event.key</h4>
+          <div class="event">
+            <div class="info-key">Tab</div>
+          </div>
+        </div>
+
+        <div class="box">
+          <h4>event.keyCode</h4>
+          <div class="event">
+            <div class="info-key">9</div>
+          </div>
+        </div>
+
+        <div class="box">
+          <h4>event.code</h4>
+          <div class="event">
+            <div class="info-key">Tab</div>
+          </div>
+        </div>
+      </div>
+ */
+
+const main = document.querySelector("main");
+
+const getInfoKey = (event) => {
+  const key = event.key;
+  const keyCode = event.keyCode;
+  const keyEventCode = event.code;
+  showInfoView(key, keyCode, keyEventCode);
+};
+
+const showInfoView = (key, keyCode, keyEventCode) => {
+  main.innerHTML = `
+  <div class="view">
+        <div class="box">
+          <h4>event.key</h4>
+          <div class="event">
+            <div class="info-key">${key === " " ? "Space" : key}</div>
+          </div>
+        </div>
+
+        <div class="box">
+          <h4>event.keyCode</h4>
+          <div class="event">
+            <div class="info-key">${keyCode}</div>
+          </div>
+        </div>
+
+        <div class="box">
+          <h4>event.code</h4>
+          <div class="event">
+            <div class="info-key">${keyEventCode}</div>
+          </div>
+        </div>
+      </div>
+  `;
+};
+
+document.addEventListener("keyup", getInfoKey);
