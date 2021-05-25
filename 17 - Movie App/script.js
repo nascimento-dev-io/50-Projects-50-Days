27b4debb9da08533140443bc6b0d09df2949b4b4
Jorge Nascimento <jorg3nascimento@gmail.com>
2021-05-25 12:18:04 -0300

    Finalizado projeto Movie App com API TMDB

--- 17 - Movie App/script.js
+++ 17 - Movie App/script.js
@@ -1,21 +1,55 @@
-const search = document.querySelector(".search");
-const containerMovies = document.querySelector(".movies-wrapper");
-const movies = Array.from(containerMovies.children);
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-let filterName = "";
+const main = document.querySelector(".movies-wrapper");
+const searchButton = document.querySelector("header input");
 
-search.addEventListener("keyup", () => {
-  const filterName = search.value.trim();
+getMovies(API_URL);
 
-  movies.forEach((el) => {
-    movieName = el.getAttribute("id");
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
 
-    if (movieName) {
-      movieName.match(filterName)
-        ? containerMovies.appendChild(el)
-        : el.remove();
-    }
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie-container");
+
+    movieEl.innerHTML = `
+    <img src="${IMG_PATH + poster_path}" alt ="${title}" class="movie-banner" />
+
+    <h3 class="title">${title}<span class="${
+      vote_average >= 8 ? "green" : vote_average >= 5 ? "orange" : "red"
+    }">${vote_average}</span></h3>
+
+    <div class="sinopse">
+      <h3>Sinopse</h3>
+    <p>${overview}</p>`;
+
+    main.appendChild(movieEl);
   });
+}
 
-  // console.log(el.getAttribute("id"));
+searchButton.addEventListener("keydown", ({ key }) => {
+  const searchTerm = searchButton.value;
+
+  if (key === "Enter") {
+    if (searchTerm && searchTerm !== "") {
+      getMovies(SEARCH_API + searchTerm);
+
+      searchButton.value = "";
+    } else {
+      window.location.reload();
+    }
+  }
 });
