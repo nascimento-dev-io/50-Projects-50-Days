5ed4a9eb8ceb4e84a61093571f160d11cb849523
Jorge Nascimento <nascimento.dev@outlook.com>
2021-06-12 13:38:48 -0300

    Finalizado o projeto Live User Filter

---
+++ 42 -  Live User Filter/README.md
@@ -0,0 +1,7 @@
+<h1 align="center"> Theme Clock </h1>
+
+<p align="center"> Projeto de criação de filtro de usuarios buscados através da Random User API.</p>
+
+</br> <hr>
+
+<p align = "center"><img src= "./.github/Live User Filter.gif"></p>
---
+++ 42 -  Live User Filter/script.js
@@ -0,0 +1,58 @@
+const url =
+  "https://randomuser.me/api/?inc=picture,name,location,page=1&results=50";
+
+const containerUsers = document.querySelector(".users");
+
+const inputFilter = document.querySelector("#search");
+let listCards = [];
+
+getUsers();
+
+async function getUsers() {
+  const request = await fetch(url);
+  const data = await request.json();
+  const users = data;
+
+  createCardUser(users);
+}
+
+function createCardUser(users) {
+  const usersArray = users.results;
+
+  usersArray.forEach((user) => {
+    const image = user.picture.medium;
+    const { first, last } = user.name;
+    const { city, country } = user.location;
+
+    const userCard = document.createElement("div");
+    userCard.classList.add("user-card");
+
+    listCards.push(userCard);
+
+    userCard.innerHTML = ` <img src=${image} alt="">
+
+      <div class="user-info">
+        <span class="name"> ${first} ${last} </span>
+        <span class="location">${city}, ${country}</span>
+      </div>
+    `;
+
+    containerUsers.appendChild(userCard);
+  });
+}
+
+inputFilter.addEventListener("input", filterUsers);
+
+function filterUsers({ target }) {
+  const nameOrLocation = target.value.toLowerCase();
+
+  const regEx = new RegExp(`^${nameOrLocation}`);
+
+  listCards.forEach((item) => {
+    if (!item.innerText.toLowerCase().match(regEx)) {
+      item.classList.add("hide");
+    } else {
+      item.classList.remove("hide");
+    }
+  });
+}
